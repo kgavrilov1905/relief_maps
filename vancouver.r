924c08aea76513bc10b1b954030680e33a1a7e05
kgavrilov1905 <kgavrilov1905@gmail.com>
2020-12-25 17:56:33 -0800

    first commit

---
+++ vancouver.r
@@ -0,0 +1,23 @@
+
+
+loadzip = tempfile()
+#download.file("https://tylermw.com/data/dem_01.tif.zip", loadzip)
+localtif = raster::raster("DEM_BC.tif")
+unlink(loadzip)
+
+elmat = raster_to_matrix(localtif)
+elmat_red = elmat[1000:5000, 3500:5996] ### Reducing the matrix
+elmat_exp = elmat[1300:4000, 3700:5500]
+
+elmat_exp %>% sphere_shade(texture = create_texture("#fff673","#55967a","#8fb28a","#55967a","#FFE5CC")) %>%
+  add_water(detect_water(elmat_exp, cutoff = 0.89444458), color = "desert") %>%
+  plot_map()
+
+elmat_exp %>% sphere_shade(texture = create_texture("#fff673","#55967a","#8fb28a","#55967a","#FFE5CC")) %>%
+  add_water(detect_water(elmat_exp, cutoff = 0.89444458), color = "lightblue") %>%
+  plot_3d(elmat_exp, zscale = 10, theta = -45, zoom = 0.5, phi = 25, windowsize = c(1000, 800))
+
+render_depth(focus = 0.6, focallength = 200, clear = TRUE) # depth
+
+render_label(elmat_exp, x = 900, y = 700, z = 1000, zscale = 10, text = "Vancouver",
+             dashed = TRUE, textsize = 1, linewidth = 5, linecolor = "red")
